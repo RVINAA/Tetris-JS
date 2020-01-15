be9ae4460746da957a5e0c03eb316f4a3ff841dc
rvinaa <juanjenosekemas@gmail.com>
2020-01-15 01:50:01 +0100

    Reestructuración DOM

---
+++ JS/cargarEstructuraDOM.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const BLOQUES = ['block-T', 'block-L2', 'block-N1', 'block-C', 'block-N2', 'block-L1', 'block-I'];
+
+    // Cabecera .header
+    let puntero = document.createElement('div');
+    puntero.className = 'header';
+    puntero.appendChild(document.createElement('p'));
+    puntero.firstElementChild.innerText = '𝑻𝑬𝑻𝑹𝑰𝑺 - @𝑺𝑰𝑮𝑴𝑨#1182';
+    document.body.appendChild(puntero);
+
+    // Contenedor #Tetris
+    puntero = document.createElement('div');
+    puntero.id = 'tetris';
+
+    // Contenedor .puntuacion
+    puntero.appendChild(document.createElement('div'));
+    puntero.firstElementChild.className = 'puntuacion';
+    puntero.firstElementChild.appendChild(document.createElement('p'));
+    puntero.firstElementChild.firstElementChild.innerText = '00000000';
+
+    // Contenedor .lineas
+    puntero.appendChild(document.createElement('div'));
+    puntero.children[1].className = 'lineas';
+    puntero.children[1].appendChild(document.createElement('p'));
+    puntero.children[1].firstElementChild.innerText = 'LINES - 000';
+
+    // Contenedor .estadisticas
+    puntero.appendChild(document.createElement('div'));
+    puntero.children[2].className = 'estadisticas';
+    puntero.children[2].appendChild(document.createElement('p'));
+    puntero.children[2].firstElementChild.innerText = 'STATISTICS';
+    puntero.children[2].appendChild(document.createElement('ul'));
+    BLOQUES.forEach( bloque => {
+        let liPuntero = document.createElement('li');
+        liPuntero.appendChild(document.createElement('img'));
+        liPuntero.firstElementChild.src = 'IMGs/' + bloque + '.png';
+        liPuntero.appendChild(document.createElement('p'));
+        liPuntero.children[1].className = bloque;
+        liPuntero.children[1].innerText = '000';
+        puntero.children[2].children[1].appendChild(liPuntero);
+    });
+
+    // Contenedor .Tablero
+    puntero.appendChild(document.createElement('div'));
+    puntero.children[3].className = 'tablero';
+    document.body.appendChild(puntero); // #Tetris.
+});
--- JS/tetris.js
+++ JS/tetris.js
@@ -1,31 +1,104 @@
 const TETRIS = (function () {
 
-    // Objeto Canvas donde representaremos nuestro juego.
+    const DIMENSION_BLOQUE = 10;
+    const HEIGHT_CANVAS = 180;
+    const WIDTH_CANVAS = 90;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   Creamos un canvas, posteriormente creamos un tablero donde guardaremos cada celda, su posición y un color a pintar.  //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     let canvasPantalla = {
         canvas : document.createElement('canvas'),
         mostrar : function() {
             document.querySelector('.tablero').appendChild(this.canvas);
             this.context = this.canvas.getContext('2d');
-            this.canvas.height = 180;
-            this.canvas.width = 90;
+            this.canvas.height = HEIGHT_CANVAS;
+            this.canvas.width = WIDTH_CANVAS;
         }
     };
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    let contadorX = -10; let contadorY = 0;
+    let tableroArray = new Array( (HEIGHT_CANVAS / DIMENSION_BLOQUE) * (WIDTH_CANVAS / DIMENSION_BLOQUE) ).fill('.');
+    tableroArray = tableroArray.map( (casilla) => {
+        if (contadorX == 80) { contadorY += DIMENSION_BLOQUE; contadorX = -(DIMENSION_BLOQUE); }
+        return new casillaTablero(contadorX += DIMENSION_BLOQUE, contadorY);
+    });
 
-    function lanzar() {
-        añadirEventoBotonIniciarParar();
-        añadirEventoDesplazamientoTeclas();
+    function casillaTablero(x, y) {
+        this.color = null;
+        this.x = x;
+        this.y = y;
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-       
+
+    function figuraCuadrado() {
+        this.color = 'yellow';
+        this.bloques = 4;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    setInterval(timerTask, 800);
+    function lanzarAplicacion() {
+        
+    }
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Botón Play / Stop con timer para poder para o reanudar el juego.
-    function añadirEventoBotonIniciarParar() {
-        let punteroPlayPause = document.querySelector('.header').getElementsByTagName('a')[0];
+
+    function añadirEventoDesplazamientoTeclas() {
+        window.addEventListener("keydown", function (event) {
+            switch(event.code) {
+                case "ArrowLeft":
+                    if (piezaValorX > topeXizq) piezaValorX -= DIMENSION_BLOQUE;
+                    break;
+                case "ArrowRight":
+                    if (piezaValorX < topeXder) piezaValorX += DIMENSION_BLOQUE;
+                    break;    
+            }
+          },false);
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function timerTask() {
+        canvasPantalla.mostrar(); // Update.
+        console.log('a')
+        new pintarBloque('red', 0, 0);
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//https://www.w3schools.com/graphics/game_controllers.asp
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function pintarBloque(color, x, y) {
+        const bloque = canvasPantalla.context;
+        bloque.fillStyle = color;
+        bloque.fillRect(x, y, DIMENSION_BLOQUE, DIMENSION_BLOQUE);  
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    return {lanzarAplicacion};
+})();
+
+
+
+
+
+
+
+
+
+
+/*
+
+    
+    function iniciarPausarJuego() {
         punteroPlayPause.addEventListener('click', onOff, false);
+        añadirEventoDesplazamientoTeclas();
 
         let temporizadorTimer;
         function onOff() {    
@@ -44,65 +117,6 @@ const TETRIS = (function () {
         }
     }
 
-    function añadirEventoDesplazamientoTeclas() {
-        window.addEventListener("keydown", function (event) {
-            switch(event.code) {
-                case "ArrowLeft":
-                    if (piezaValorX > topeXizq) piezaValorX -= 10;
-                    break;
-                case "ArrowRight":
-                    if (piezaValorX < topeXder) piezaValorX += 10;
-                    break;    
-            }
-          },false);
-    }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // Timer para el Juego.
-    function timerTask() {
-        canvasPantalla.mostrar(); // Pintamos el canvas.
-        let cuadrado = new pintarBloque('red', piezaValorX, piezaValorY);
-        detectarColisionInferior();
-    }
-
-    function detectarColisionInferior() {
-        if (piezaValorY < 170) {
-            piezaValorY += 10;
-        }
-    }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//https://www.w3schools.com/graphics/game_controllers.asp
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-    function pintarBloque(color, x, y) {
-        this.height = 10;
-        this.width = 10;
-        this.x = x;
-        this.y = y;
-        let bloque = canvasPantalla.context;
-        bloque.fillStyle = color;
-        bloque.fillRect(this.x, this.y, this.width, this.height);
-    }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    return {lanzar};
-})();
-
-
-
-
-
-
-
-
-
-
-/*
 
     const heredar = function(padre, hijo) {
         hijo.prototype = Object.create(padre.prototype);
